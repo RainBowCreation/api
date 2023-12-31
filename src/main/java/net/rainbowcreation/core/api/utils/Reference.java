4713a8b0fa032470a6b2394395574f32ebde4f61
Nathaniel Song <35866098+VocanicZ@users.noreply.github.com>
2023-12-31 15:30:21 +0700

    Update Reference.java

--- src/main/java/net/rainbowcreation/core/api/utils/Reference.java
+++ src/main/java/net/rainbowcreation/core/api/utils/Reference.java
@@ -9,7 +9,7 @@ public final class Reference {
     }
 
     public static final String NAME = "api";
-    public static final String VERSION = "1.0.1-SNAPSHOT";
+    public static final String VERSION = "1.0.1";
 
 
     public static final List<String> HEADER = Arrays.asList("######################################################################################",
